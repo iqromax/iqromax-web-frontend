d0fa67a09572c5b82855357bef50076db9fe6b39
Mirshoxid <mirshoxidtoshtemirov04@gmail.com>
2026-05-18 00:21:06 +0500

    chore: split mono repository, untracking backend directory from frontend repository

--- backend/ads/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Ad, Feature, FeatureDetail
-
-admin.site.register(Ad)
-admin.site.register(Feature)
-admin.site.register(FeatureDetail)
--- backend/create_superuser.py
+++
@@ -1,6 +0,0 @@
-from django.contrib.auth.models import User
-if not User.objects.filter(username='rahmatulla').exists():
-    User.objects.create_superuser('rahmatulla', 'rahmatulla@iqromax.uz', 'rahmatulla123')
-    print("Superuser created successfully.")
-else:
-    print("Superuser already exists.")
